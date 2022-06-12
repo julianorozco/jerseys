a40a5380f5f11ce94dae5af7a4fcbc517ef073bf
Julian Orozco <orozcojulian23@gmail.com>
2022-06-12 20:49:37 -0300

    resumen

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -13,13 +13,13 @@ const Cart = () => {
           <div className="row d-flex justify-content-center align-items-center h-100">
             <div className="col-12">
               <div className="card card-registration card-registration-2">
-                {cart.map((prod) => {
-                  return (
-                    <div key={prod.id}>
-                      <div className="card-body p-0">
-                        <div className="row">
-                          <div>
-                            <div className="p-3 shadow">
+                <div className="card-body p-0">
+                  <div className="row">
+                    <div className="col-lg-8">
+                      {cart.map((prod) => {
+                        return (
+                          <div key={prod.id}>
+                            <div className="p-5 shadow">
                               <div className="row d-flex justify-content-between align-items-center">
                                 <Link to={`/detail/${prod.id}`} className="col-md-2 col-lg-2 col-xl-2">
                                   <img src={prod.pictureUrl} alt="" className="w-75 img-thumbnail" />
@@ -52,11 +52,33 @@ const Cart = () => {
                               </div>
                             </div>
                           </div>
+                        );
+                      })}
+                    </div>
+                    <div className="col-lg-4 bg-grey">
+                      <div className="p-5">
+                        <h3 className="fw-bold mb-5 mt-2 pt-1">Resumen</h3>
+                        <hr className="my-4"></hr>
+                        <div className="d-flex justify-content-between mb-4">
+                          <h5 className="text-uppercase">Items</h5>
+                          <h5>$ 132</h5>
                         </div>
+                        <div className="d-flex justify-content-between mb-4">
+                          <h5 className="text-uppercase">Envio</h5>
+                          <h5>Gratis</h5>
+                        </div>
+                        <hr className="my-4"></hr>
+                        <div className="d-flex justify-content-between mb-5">
+                          <h5 className="text-uppercase">Total</h5>
+                          <h5>$ 132</h5>
+                        </div>
+                        <button type="button" className="btn btn-dark btn-block btn-lg" data-mdb-ripple-color="dark">
+                          Comprar
+                        </button>
                       </div>
                     </div>
-                  );
-                })}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
